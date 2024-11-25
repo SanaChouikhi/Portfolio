12804ee190a8cf2e1d993ee42e651648580a2e5b
Sana <sanachouikhi1999@gmail.com>
2024-11-25 21:18:13 +0100

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,13 @@ import tailwind from '@astrojs/tailwind';
 
 import sitemap from "@astrojs/sitemap";
 
+const LIVE_URL="https://SanaChouikhi.github.io/Portfolio";
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [react(), tailwind(), sitemap()]
+  integrations: [react(), tailwind(), sitemap()],
+  site:LIVE_URL,
 });
